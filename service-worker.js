b25f942cbfe6f78732ae3e324e4b4b409a7d6523
Aryak Mohanty <92839189+Aryak-Mohanty@users.noreply.github.com>
2023-10-03 23:28:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,23 @@
 // service-worker.js
 
-const CACHE_NAME = 'searchpage-cache-v1';
-// List of resources to cache
-const resourcesToCache = [
-  '/',
-  'index.html',
-  'icon.png',
-  // Add other assets you want to cache here...
-];
+const CACHE_NAME = 'weather-app-cache-v1';
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          '/',
+          'index.html',
+          'icon.png',
+          // ... other assets ...
+        ])
+        .catch((error) => {
+          console.error('Cache addAll error:', error);
+        });
+      })
+  );
+});
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
